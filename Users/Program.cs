b6c18b4ded298244acffcf8e3fa6c1b61094ad39
dluppoli <davide@davideluppoli.it>
2022-10-20 17:12:52 +0200

    Add project files.

---
+++ Users/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Users.Models;
+
+namespace Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = Controller.Users.GetAll();
+            //PrintTable(users);
+
+            //Stampare tutti i maschi
+            List<User> maschi = users.FindAll(u => u.Gender == "male");
+            PrintTable(maschi);
+
+
+            Console.ReadLine();
+        }
+
+        static void PrintTable(List<User> users)
+        {
+            foreach (string s in Controller.Users.FormatAsTable(users))
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
