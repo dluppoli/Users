1d3397038ef4adfbe99d4e725541f297e26a83eb
dluppoli <davide@davideluppoli.it>
2022-10-20 17:59:33 +0200

    Aggiunta esercizi

--- Users/Program.cs
+++ Users/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Users.Models;
 
 namespace Users
@@ -9,11 +10,18 @@ namespace Users
         static void Main(string[] args)
         {
             List<User> users = Controller.Users.GetAll();
-            //PrintTable(users);
+            PrintTable(users);
 
             //Stampare tutti i maschi
             List<User> maschi = users.FindAll(u => u.Gender == "male");
-            PrintTable(maschi);
+            //PrintTable(maschi);
+
+            //Stampare tutti gli utenti della california (state==CA)
+            //Stampare tutti gli utenti di origine giapponese (mail che finisce co jp)
+            //Stampare tutti gli utenti con password <= di 8 caratteri
+            //Contare quanti utenti sono nati nel 1979
+            //Stampare tutti gli utenti con età > di 50 anni (calcolata da BirthDate)
+            //Stampare tutti gli utenti che lavorano per amazon
 
 
             Console.ReadLine();
