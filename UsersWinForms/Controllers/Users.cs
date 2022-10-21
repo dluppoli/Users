dbd6613de18b5b72f778a1c9d169d1fa1d9354f4
dluppoli <davide@davideluppoli.it>
2022-10-21 12:08:33 +0200

    Progetto UserWinForms con Models e Controllers

---
+++ UsersWinForms/Controllers/Users.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersWinForms.Models;
+
+namespace UsersWinForms.Controllers
+{
+    internal static class Users
+    {
+        public static List<User> GetAll()
+        {
+            string json = File.ReadAllText(@".\Models\database.json");
+
+            return JsonConvert.DeserializeObject<List<User>>(json);
+        }
+
+        public static string[] FormatAsTable(List<User> users)
+        {
+            string[] table = new string[users.Count+4];
+
+            int i = 0;
+
+            table[i++] = new String('-', 170);
+            table[i++] = string.Format("|{0,3}|{1,10}|{2,12}|{3,3}|{4,7}|{5,27}|{6,13}|{7,13}|{8,11}|{9,30}|{10,15}|{11,6}|{12,6}|",
+                "Id", "FirstName","LastName", "Age", "Gender", "Email", "Username", "Password", "BirthDate", "Address","City", "Zip", "State");
+            table[i++] = new String('-', 170);
+
+            foreach (User u in users)
+            {
+                string s = string.Format("|{0,3}|{1,10}|{2,12}|{3,3}|{4,7}|{5,27}|{6,13}|{7,13}|{8,11}|{9,30}|{10,15}|{11,6}|{12,6}|",
+                    u.Id, u.FirstName, u.LastName, u.Age, u.Gender, u.Email, u.Username, u.Password, u.BirthDate.ToShortDateString(), u.Address.Address, u.Address.City, u.Address.PostalCode, u.Address.State);
+
+                table[i++] = s;
+            }
+            table[i++] = new String('-', 170);
+            return table;
+        }
+    }
+}
