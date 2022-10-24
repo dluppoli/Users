ccf0b4e086fdca8dd2390d89c86b28b7da8e0f5f
dluppoli <davide@davideluppoli.it>
2022-10-24 21:06:12 +0200

    Logins controller

---
+++ UsersWinForms/Controllers/Logins.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersWinForms.Models;
+
+namespace UsersWinForms.Controllers
+{
+    public static class Logins
+    {
+        private static List<Login> _logins;
+        public static List<Login> GetAll()
+        {
+            if (_logins == null)
+            {
+                string json = File.ReadAllText(@".\Models\logins.json");
+                _logins = JsonConvert.DeserializeObject<List<Login>>(json);
+            }
+            return _logins;
+        }
+
+        public static List<Login> FindAll(Predicate<Login> condizione)
+        {
+            return GetAll().FindAll(condizione);
+        }
+
+        public static Login Find(Predicate<Login> condizione)
+        {
+            return GetAll().Find(condizione);
+        }
+
+        public static void Add(Login u)
+        {
+            GetAll().Add(u);
+        }
+    }
+}
