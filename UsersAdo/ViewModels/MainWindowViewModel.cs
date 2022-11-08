28eff9881a73422db97d3db518d9e330d9cd1948
dluppoli <davide@davideluppoli.it>
2022-11-08 17:07:55 +0100

    WPF ADO.NET

--- UsersAdo/ViewModels/MainWindowViewModel.cs
+++ UsersAdo/ViewModels/MainWindowViewModel.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
+using UsersClassLibrary.Controllers;
+using UsersClassLibrary.Models;
 
 namespace UsersAdo.ViewModels
 {
@@ -14,7 +18,50 @@ namespace UsersAdo.ViewModels
         
         internal void CreateTables()
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = @"create table Users(
+                            Id INT PRIMARY KEY,
+                            FirstName VARCHAR(1000) NOT NULL,
+                            LastName VARCHAR(1000) NOT NULL,
+                            Age INT NOT NULL,
+	                        Gender VARCHAR(10) NOT NULL,
+                            Email VARCHAR(1000) NOT NULL,
+                            Username VARCHAR(1000) NOT NULL,
+                            Password VARCHAR(1000) NOT NULL,
+                            BirthDate DATETIME NOT NULL,
+	                        Address VARCHAR(1000) NOT NULL,
+                            City VARCHAR(1000) NOT NULL,
+                            PostalCode VARCHAR(1000) NOT NULL,
+                            State VARCHAR(1000) NOT NULL)";
+                    command.Connection = connection;
+                    command.ExecuteNonQuery();
+
+
+                    command.CommandText = @"CREATE TABLE Logins(
+                            UserId INT NOT NULL,
+                            Date DATETIME NOT NULL,
+                            LoginSuccessful BIT NOT NULL,
+                            Note VARCHAR(1000),
+                            FOREIGN KEY(UserId) REFERENCES Users(Id)
+                        )";
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Tabelle create con successo");
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("Tabelle già esistenti");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Errore generico");
+                }
+            }
         }
 
         internal void DeleteFromTables()
@@ -70,7 +117,61 @@ namespace UsersAdo.ViewModels
 
         internal void LoadDataOnTables()
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    List<User> users = Users.GetAll();
+
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = connection;
+
+                    foreach (User u in users)
+                    {
+                        command.CommandText = "INSERT INTO Users VALUES(";
+                        command.CommandText += u.Id.ToString() + ",";
+                        command.CommandText += "'" + u.FirstName.Replace("'","''") + "',";
+                        command.CommandText += "'" + u.LastName.Replace("'", "''") + "',";
+                        command.CommandText += u.Age + ",";
+                        command.CommandText += "'" + u.Gender.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.Email.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.Username.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.Password.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.BirthDate.ToString("yyyy-MM-dd HH:mm:ss") + "',";
+                        command.CommandText += "'" + u.Address.Address.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.Address.City.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.Address.PostalCode.Replace("'", "''") + "',";
+                        command.CommandText += "'" + u.Address.State.Replace("'", "''") + "'";
+                        command.CommandText += ")";
+
+                        command.ExecuteNonQuery();
+                    }
+
+
+                    List<Login> logins = Logins.GetAll();
+                    foreach(Login l in logins)
+                    {
+                        command.CommandText = "INSERT INTO Logins VALUES(";
+                        command.CommandText += l.UserId.ToString() + ",";
+                        command.CommandText += "'" + l.Date.ToString("yyyy-MM-dd HH:mm:ss") + "',";
+                        command.CommandText += l.LoginSuccessfull ? "1," : "0,";  
+                        command.CommandText += "'" + l.Notes?.Replace("'", "''") + "'";
+                        command.CommandText += ")";
+
+                        command.ExecuteNonQuery();
+                    }
+
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("Errore Sql");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Errore generico");
+                }
+            }
         }
     }
 }
