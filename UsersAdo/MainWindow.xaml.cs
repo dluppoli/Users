e97528e1461a397a5b6005d40413195ab5238ed8
dluppoli <davide@davideluppoli.it>
2022-11-08 16:01:01 +0100

    UsersWPF Ado

---
+++ UsersAdo/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UsersAdo.ViewModels;
+
+namespace UsersAdo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+
+        private void DropTables_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Droptables();
+        }
+        private void CreateTables_Click(object sender, RoutedEventArgs e)
+        {
+            vm.CreateTables();
+        }
+        private void DeleteFromTables_Click(object sender, RoutedEventArgs e)
+        {
+            vm.DeleteFromTables();
+        }
+        private void LoadDataOnTables_Click(object sender, RoutedEventArgs e)
+        {
+            vm.LoadDataOnTables();
+        }
+    }
+}
---
+++ UsersAdo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UsersAdo.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        private string connectionString = @"Server=E80\SQLEXPRESS;Database=Users;Integrated Security=True;TrustServerCertificate=True";
+        
+        internal void CreateTables()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DeleteFromTables()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "DELETE FROM Logins; DELETE FROM Users";
+                    command.Connection = connection;
+                    int righe = command.ExecuteNonQuery();
+                    if( righe==0)
+                        MessageBox.Show("Tabelle già vuote");
+                    else
+                        MessageBox.Show("Tabelle svuotate con successo");
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("Errore Sql");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Errore generico");
+                }
+            }
+        }
+
+        internal void Droptables()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "DROP TABLE Logins; DROP TABLE Users";
+                    command.Connection = connection;
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Tabelle cancellate con successo");
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("Tabelle non esistenti");
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Errore generico");
+                }
+            }
+        }
+
+        internal void LoadDataOnTables()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
